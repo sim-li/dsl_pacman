91cf227dedf288f0b58743c0619bcc704fc825ff
Simon Lischka <simon@a-studios.org>
2017-01-12 17:44:06 +0100

    Offical JS object syntax

--- pacman/figures/ghost.js
+++ pacman/figures/ghost.js
@@ -1,3 +1,25 @@
 define(["underscore"], function(_) {
+    var Ghost = function(iv) {
+        var ins=iv;
 
+        function myCool() {
+            console.log(ins);
+        };
+
+        function doAMove() {
+            x = x + 10;
+        };
+
+        function setIns(newIns){
+            ins = newIns;
+        };
+
+        return {
+            myCool: myCool,
+            setIns: setIns
+        };
+    };
+
+
+    return Ghost;
 });
--- pacman/game.js
+++ pacman/game.js
@@ -1,22 +1,37 @@
-define(["underscore"], function(_) {
+define(["underscore", "figures/ghost", "figures/pac"], function(_, Ghost, Pac) {
     // Demonstrates pattern for creating class that can be instantiated with `new`.
     // Prototype has to be returned by module.
 
-    var Game = function() {};
+    var Game = function() {
+        function run() {
+            var ghost = new Ghost("hulalala");
 
-    Game.prototype = {
-        run: function() {
+            //This will fail because private variable. Access via setter!
+            //ghost.ins ="manipulation";
+            ghost.setIns("mani");
+
+            console.log(ghost.ins);
+            ghost.myCool();
+
+
             console.log("Pacman says:");
+
             _.each(["hello", "world"], function(w) {
                 console.log(w);
             });
-        },
 
-        // Dummy function to demonstrate unit testing with jasmine.
-        demoValue: function() {
+        };
+
+        function demoValue() {
             return 123;
+        };
+
+        return {
+            run: run,
+            demoValue: demoValue
         }
     };
 
     return Game;
+
 });
