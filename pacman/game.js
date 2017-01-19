bd91aab218ab514d925fea1d66df652a0c4a04ec
s67176 <s67176@beuth-hochschule.de>
2017-01-19 14:56:33 +0100

    pac

--- pacman/game.js
+++ pacman/game.js
@@ -12,7 +12,7 @@ define(["underscore", "jquery", "figures/ghost", "figures/pac", "levels/level1"]
         canvas.setAttribute("height", (BLOCKSIZE * ROWS) + "px");
         canvas_pac.setAttribute("width", (BLOCKSIZE * COLS) + "px");
         canvas_pac.setAttribute("height", (BLOCKSIZE * ROWS) + "px");
-        
+
 
         var ctx = canvas.getContext('2d');
         var ctx_pac = canvas_pac.getContext('2d');
@@ -21,6 +21,10 @@ define(["underscore", "jquery", "figures/ghost", "figures/pac", "levels/level1"]
         var fruitImg;
         var wallImg;
         var pointImg;
+
+        var gridx = 10;
+        var gridy = 10;
+        
 
         function init() {
             $('#text').click(function () {
@@ -32,7 +36,7 @@ define(["underscore", "jquery", "figures/ghost", "figures/pac", "levels/level1"]
             imgLoader = new BulkImageLoader();
             imgLoader.addImage("cherry.png", "cherry");
             imgLoader.addImage("wall.png", "wall");
-            imgLoader.addImage("Bitcoin.png","Bitcoin");
+            imgLoader.addImage("Bitcoin.png", "Bitcoin");
             imgLoader.onReadyCallback = onImagesLoaded;
             imgLoader.loadImages();
             ctx.clearRect(0, 0, 600, 600);
@@ -43,30 +47,54 @@ define(["underscore", "jquery", "figures/ghost", "figures/pac", "levels/level1"]
             wallImg = imgLoader.getImageAtIndex(1);
             pointImg = imgLoader.getImageAtIndex(2);
             drawBoard();
-            var i = 0;
-            setInterval(function(){
-                var WIDTH = 600;
-                var HEIGHT = 600; 
-                
-                ctx_pac.clearRect(0, 0, WIDTH, HEIGHT);
-                
-                var oneRowInPx = HEIGHT / ROWS;
-                var oneColInPx = WIDTH / COLS;
-                
-                0123
-                202122
-                
-                min(i, )
-                
-                10 20 30 40 50 
-                
-                ctx_pac.drawImage(wallImg, i, 10 * BLOCKSIZE, BLOCKSIZE, BLOCKSIZE);
-                i+=1;
+            drawPac();
+//            var i = 0;
+            setInterval(function () {
+              
+
+                ctx_pac.clearRect(0, 0, 600, 600);
+                drawPac();
+
             }, 25);
         }
-        
-        function checkMove(){
+
+        //zeichnet den spielstein
+        function drawPac()
+        {
+            var drawX = gridx;
+            var drawY = gridy;
+            var alive = 1;
+            var hungry = 2;
+            var dead = 3;
+            var state = alive;
             
+            ctx_pac.drawImage(wallImg, 0, 0, BLOCKSIZE, BLOCKSIZE, drawX * BLOCKSIZE, drawY * BLOCKSIZE, BLOCKSIZE, BLOCKSIZE);
+        }
+//funktion Ã¼berprÃ¼ft mÃ¶gliche bewegeung nach links, recht und unten
+        function checkMove(xpos, ypos)
+        {
+            var result = true;
+            var newx = xpos;
+            var newy = ypos;
+
+            if (newx < 0 || newx >= COLS)
+            {
+                result = false;
+            }
+
+            if (level.map[newy] != undefined && level.map[newy][newx] != 0)
+            {
+                result = false;
+            }
+            
+            
+
+            if (newy < 0 || newy > ROWS)
+            {
+                result = false;
+            }
+
+            return result;
         }
 
         function drawBoard() {
@@ -74,14 +102,14 @@ define(["underscore", "jquery", "figures/ghost", "figures/pac", "levels/level1"]
                 for (var c = 0; c < COLS; c++) {
                     if (level.map[r][c] === 3) {
 
-                        ctx.drawImage(wallImg, c * BLOCKSIZE , r * BLOCKSIZE, BLOCKSIZE, BLOCKSIZE);
+                        ctx.drawImage(wallImg, c * BLOCKSIZE, r * BLOCKSIZE, BLOCKSIZE, BLOCKSIZE);
 
                     }
                     else if (level.map[r][c] === 2) {
                         ctx.drawImage(fruitImg, 0, 0, BLOCKSIZE, BLOCKSIZE, c * BLOCKSIZE, r * BLOCKSIZE, BLOCKSIZE, BLOCKSIZE);
                     }
                     else if (level.map[r][c] === 1) {
-                        ctx.drawImage(pointImg, 0, 0, BLOCKSIZE, BLOCKSIZE, c * BLOCKSIZE+7.5, r * BLOCKSIZE+7.5, BLOCKSIZE/2, BLOCKSIZE/2);
+                        ctx.drawImage(pointImg, 0, 0, BLOCKSIZE, BLOCKSIZE, c * BLOCKSIZE + 7.5, r * BLOCKSIZE + 7.5, BLOCKSIZE / 2, BLOCKSIZE / 2);
                     }
                 }
             }
@@ -90,7 +118,8 @@ define(["underscore", "jquery", "figures/ghost", "figures/pac", "levels/level1"]
         function run() {
             isGameOver = false;
             console.log("Got run");
-        };
+        }
+        ;
 
 
         function getInput(e) {
@@ -104,20 +133,30 @@ define(["underscore", "jquery", "figures/ghost", "figures/pac", "levels/level1"]
                     case 37:
 //                            if (checkMove(curPiece.gridx - 1, curPiece.gridy, curPiece.curState))
 //                                curPiece.gridx--;
-                            console.log("left");
+                        console.log("left");
+                          e.preventDefault();
+                    if (checkMove(gridx - 1, gridy))
+                        gridx--;
+                       
                         break;
                     case 39:
 //                            if (checkMove(curPiece.gridx + 1, curPiece.gridy, curPiece.curState))
 //                                curPiece.gridx++;
-                            console.log("right");
+                        console.log("right");
+                        if (checkMove(gridx + 1, gridy))
+                        gridx++;
+                        
                         break;
                     case 38:
-                            console.log("up");
+                        console.log("up");
+                        if (checkMove(gridx, gridy - 1))
+                        gridy--;
+                        
                         break;
                     case 40:
-//                            if (checkMove(curPiece.gridx, curPiece.gridy + 1, curPiece.curState))
-//                                curPiece.gridy++;
-                            console.log("down");
+                           if (checkMove(gridx, gridy + 1))
+                        gridy++;
+                        console.log("down");
                         break;
                 }
             }
