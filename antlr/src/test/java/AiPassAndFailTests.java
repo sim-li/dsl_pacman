bae11b17a387356a8119376923b601b366ed4991
Simon Lischka <simon@a-studios.org>
2017-01-23 21:32:53 +0100

    Update testcase

--- antlr/src/test/java/AiPassAndFailTests.java
+++ antlr/src/test/java/AiPassAndFailTests.java
@@ -2,9 +2,57 @@ import junit.framework.TestCase;
 import java.io.IOException;
 
 public class AiPassAndFailTests extends TestCase {
-    final static String WORKING_DIRECTORY = System.getProperty("user.dir") + "/src/test/java/testAis";
+    final String USER_DIR = System.getProperty("user.dir");
+    final String WORKING_DIRECTORY = USER_DIR + "/src/test/java/testAis";
+    boolean recompiledGrammar = false;
 
-    public void testBasic() throws IOException {
-        PacmanParserExecutor.parseAi(WORKING_DIRECTORY + "/random.ai");
+    public void testRandomWithNestedFilterFreeN() throws IOException {
+        recompileGrammar();
+        parseAndAssertNoError("randomFilterFreeN");
     }
+
+    public void testIfStatementWithMoves() throws IOException {
+        recompileGrammar();
+        parseAndAssertNoError("ifStatementWithMoves");
+    }
+
+    public void recompileGrammar() {
+        if (recompiledGrammar) {
+            return;
+        }
+        if (recompiledGrammar == false) {
+            recompiledGrammar = true;
+        }
+        try {
+            Process p = Runtime.getRuntime().exec(USER_DIR + "/build.sh");
+            p.waitFor();
+        } catch (InterruptedException interruptedException) {
+            System.out.println("Got interrupted exception running grammar compile script");
+        } catch (IOException ioException) {
+            System.out.println("Got io exception running grammar compile script");
+        }
+    }
+
+    public void parseAndAssertNoError(String name) {
+        try {
+            final ParsingResult parsingResult = PacmanParserExecutor.parseAi(buildPath(name));
+            assertEquals(parsingResult.getNumberOfSyntaxErrors(), 0);
+        } catch (IOException ioEx) {
+            fail("IO Exception reading test " + name);
+        }
+    }
+
+    public void parseAndAssertError(String name) {
+        try {
+            final ParsingResult parsingResult = PacmanParserExecutor.parseAi(buildPath(name));
+            assertTrue(parsingResult.getNumberOfSyntaxErrors() > 0);
+        } catch (IOException ioEx) {
+            fail("IO Exception reading test " + name);
+        }
+    }
+
+    public String buildPath(String name) {
+        return WORKING_DIRECTORY + "/" + name + ".ai";
+    }
+
 }
